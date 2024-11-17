daba9ac663cc62de3a8175755ead8211ba2d3abd
oulibouli <oulikhanow.nico@gmail.com>
2024-11-17 16:02:51 +0100

    Establishing websockets

--- backend/src/main/java/com/yc_your_way/chat/controller/ChatController.java
+++ backend/src/main/java/com/yc_your_way/chat/controller/ChatController.java
@@ -1,17 +1,19 @@
 package com.yc_your_way.chat.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.yc_your_way.chat.model.Message;
 
 
 @Controller
 public class ChatController {
     @MessageMapping("/chat/{roomId}")
     @SendTo("/topic/{roomId}")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public Message handleMessage(@DestinationVariable String roomId, Message message) {
+        System.out.println(message.getContent());
+        return message;
     }
-    
 }
---
+++ backend/src/main/java/com/yc_your_way/chat/model/Message.java
@@ -0,0 +1,9 @@
+package com.yc_your_way.chat.model;
+
+import lombok.Data;
+
+@Data
+public class Message {
+    private String sender;
+    private String content;
+}
