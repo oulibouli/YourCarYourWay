06acaac3aa7ee1a05bb99c3470f108657bba1225
oulibouli <oulikhanow.nico@gmail.com>
2024-11-18 12:00:24 +0100

    Add jsonproperty to model Message

--- backend/src/main/java/com/yc_your_way/chat/model/Message.java
+++ backend/src/main/java/com/yc_your_way/chat/model/Message.java
@@ -1,9 +1,13 @@
 package com.yc_your_way.chat.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class Message {
+    @JsonProperty("sender")
     private String sender;
+    @JsonProperty("content")
     private String content;
 }
